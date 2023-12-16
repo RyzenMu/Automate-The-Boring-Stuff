061a8b7b4251facb3fff73c47a514b9e029a09d8
RyzenMu <muruga13in2022@gmail.com>
2023-12-16 06:02:43 +0530

    files append

---
+++ file_write.py
@@ -0,0 +1,6 @@
+file_hello = open("D:\\AutomateTheBoringStuff\\files_hello1.txt", "w")
+file_hello.write("This is s a string\n")
+file_hello.write("This is s a string1\n")
+file_hello.write("This is s a string2\n")
+file_hello.write("This is s a string3\n")
+file_hello.close()
---
+++ files_append.py
@@ -0,0 +1,4 @@
+file_hello = open("D:\\AutomateTheBoringStuff\\files_hello1.txt", "a")
+for i in range(10):
+    file_hello.write("bacon is delicious \n")
+file_hello.close()    
