bc69ecad0cf66b11160ea9f3461a545efb795c59
RyzenMu <muruga13in2022@gmail.com>
2023-12-23 18:51:58 +0530

    gui screen shot

---
+++ gui_screenshot.py
@@ -0,0 +1,5 @@
+import pyautogui
+
+print(pyautogui.screenshot('C:\\Users\\x\\Desktop\\example1.png')) 
+print(pyautogui.locateOnScreen('C:\\Users\\x\\Desktop\\key7.png'))
+print(pyautogui.locateCenterOnScreen('C:\\Users\\x\\Desktop\\key7.png'))
